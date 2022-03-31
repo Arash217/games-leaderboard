b09e673587bf940828ff849d762eb27fef675639
Arash Paknezad <a.paknezad@hotmail.com>
2022-03-31 11:19:15 +0200

    feat: handle join competition client side

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import '../styles/index.scss'
 import Layout from '../components/Layout'
 import CompetitionsProvider from '../store/CompetitionsProvider'
+import PlayerRanksProvider from '../store/PlayerRanksProvider'
 import { UserProvider } from '@auth0/nextjs-auth0'
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
       <CompetitionsProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <PlayerRanksProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </PlayerRanksProvider>
       </CompetitionsProvider>
     </UserProvider>
   )
